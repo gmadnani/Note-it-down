e0cbcdafeec88250e557c44e435e555c79e67412
Girish Madnani <girishmadnani05@gmail.com>
2022-03-12 17:22:35 +0000

    get notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,11 @@
+const fs = require("fs");
+
 const router = require('express').Router();
+
+router.get("/notes", (req, res) => {
+
+        let data = JSON.parse(fs.readFileSync("./db/db.json"));
+        res.json(data);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
@@ -8,14 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
+app.use(express.static("public"));
 
-
-app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(PORT, () => {
-    console.log(`API server is ready on port ${PORT}!`);
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
 });
