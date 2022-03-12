062f7f6ba5f594aa3cbe8bc5d312649c839c0563
Girish Madnani <girishmadnani05@gmail.com>
2022-03-12 17:37:51 +0000

    post/save notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,20 @@
 const fs = require("fs");
+const uuid = require('uuidv4');
 
 const router = require('express').Router();
 
 router.get("/notes", (req, res) => {
+    let data = JSON.parse(fs.readFileSync("./db/db.json"));
+     res.json(data);
+});
 
-        let data = JSON.parse(fs.readFileSync("./db/db.json"));
-        res.json(data);
+router.post("/notes", (req, res) => {
+    const note = req.body;
+    note.id = uuid;
+    let data = JSON.parse(fs.readFileSync("./db/db.json"));
+    data.push(note);
+    fs.writeFileSync('./db/db.json', JSON.stringify(data));
+    res.json(data);
 });
 
 module.exports = router;
